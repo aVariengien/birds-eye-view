47feaf41245759aad38f32cc7d046559ac6ab752
aVariengien <alexandre.variengien@gmail.com>
2024-12-07 17:00:45 +0100

    add red to setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read the contents of requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="birds_eye_view",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        # any required dependencies
-    ],
-    # other setup options
+    install_requires=requirements,
 )
